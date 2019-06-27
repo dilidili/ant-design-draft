ab4bc3acbfdfed0a804a409f7272ceacf1a9c2da
Wensheng Xu <xws@superid.cn>
2019-06-27 15:20:42 +0800

    [feat] @Select @DatePicker

--- src/pages/build/mentions.ts
+++ src/pages/build/mentions.ts
@@ -5,15 +5,41 @@ export type Mention = {
   insertAsText: boolean,
 }
 
-const mentions: [Mention] = [
+const mentions: Array<Mention> = [
   {
     shortname: 'Input',
-    title: 'Form item label: <Input />',
+    title: 'Form item label: |--- placeholder ---|',
     name: `{
         name: 'email',
         label: 'E-mail',
         type: 'Input',
         rules: ['required', 'email'],
+      },`,
+    insertAsText: true,
+  },
+  {
+    shortname: 'Select',
+    title: 'Form item label: |--- option --- v |',
+    name: `{
+        name: 'attendees',
+        label: 'Attendees',
+        type: 'Select',
+        rules: ['required'],
+      },`,
+    insertAsText: true,
+  },
+  {
+    shortname: 'DatePicker',
+    title: 'Form item label: |--- YYYY-MM-DD HH:mm:ss --- v|',
+    name: `{
+        name: 'birthday',
+        label: 'Birthday',
+        type: 'DatePicker',
+        rules: ['required'],
+        props: {
+          showTime: true,
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }
       },`,
     insertAsText: true,
   },
