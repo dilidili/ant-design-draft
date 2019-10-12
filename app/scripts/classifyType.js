740de2bc2ed9a49bcee91e2d91167537e5722b54
Wensheng Xu <xws@superid.cn>
2019-10-12 15:33:05 +0800

    [feat] new version classify algorithm.

--- app/scripts/classifyType.js
+++ app/scripts/classifyType.js
@@ -1,19 +1,117 @@
-const { componentFeature, getFeatures, distanceBetween } = require('./componentFeatures');
+const cv = require("opencv4nodejs");
 
-// classify form item type:
-//  1. normalize and calculate feature points
-//  2. get the distance
-//  3. determine use which type
-const classifyType = (contour) => {
-  const features = getFeatures(contour);
-  const featureDistance = Object.keys(componentFeature).map(component => ({
-    component,
-    distance: distanceBetween(componentFeature[component], features),
-  })).sort((a, b) => a.distance - b.distance);
+const White = new cv.Vec(255, 255, 255);
 
-  contour.type = featureDistance[0].component;
+const ButtonType = 1;
+const CheckboxType = 2;
+const InputType = 3;
+const LabelType = 4;
+const PrefixInputType = 5;
 
-  return contour;
+const containRect = (a, b) => {
+  return b.x >= a.x && b.y >= a.y && (b.x + b.width) <= (a.x + a.width) && (b.y + b.height) <= (a.y + a.height);
+}
+
+const isEqualRect = (a, b) => {
+  return a.x === b.x && a.y === b.y && a.width === b.width && a.height === b.height;
+}
+
+const classifyType = (forItemContours, canny, contours, gray) => {
+  const types = new Array(forItemContours.length);
+
+  // Detect text content.
+  const hists = forItemContours.map((contour, index) => {
+    const mask = new cv.Mat(canny.rows, canny.cols, canny.type, 0);
+    const padding = 2;
+    const stripBorder = new cv.Rect(contour.x + padding, contour.y + padding, contour.width - 2 * padding, contour.height - 2 * padding);
+    mask.drawRectangle(stripBorder, White, cv.FILLED);
+    const hist = cv.calcHist(canny, [{
+      channel: 0,
+      bins: 2,
+      ranges: [0, 256],
+    }], mask)
+
+    cv.imwrite(`label${index}.png`, mask);
+
+    const hData = hist.getDataAsArray();
+    return hData[1][0] / hData[0][0];
+  });
+
+  forItemContours.forEach((contour, index) => {
+    let childrenContour = contours.filter(v => !isEqualRect(contour, v) && containRect(contour, v));
+    childrenContour = childrenContour.filter(v => v.x - contour.x + v.y - contour.y + contour.height - v.height + contour.width - v.width > 20);
+    childrenContour = childrenContour.filter(v => !childrenContour.some(w => !isEqualRect(w, v) && containRect(w, v)));
+    childrenContour = childrenContour.map(v => {
+      return new cv.Rect(v.x - contour.x, v.y - contour.y, v.width, v.height)
+    });
+
+    const crop = canny.getRegion(contour);
+    for (let i = 0; i < childrenContour.length; i++) {
+      const contour = childrenContour[i];
+      crop.drawRectangle(contour, White, 1);
+    }
+
+    // Label type or checkbox.
+    if (hists[index] > 0.1) {
+      if (childrenContour.length > 1) {
+        types[index] = CheckboxType;
+      } else {
+        types[index] = LabelType;
+      }
+
+      return;
+    }
+
+    const col = contour.width / 24;
+    const layout = childrenContour.map((child) => {
+      const span = Math.round(child.width / col);
+      const offset = Math.round(child.x / col);
+
+      return {
+        span,
+        offset,
+      }
+    });
+
+    if (childrenContour.length === 1) {
+      const leftPadding = layout[0].offset;
+      const rightPadding = 24 - layout[0].offset - layout[0].span;
+
+      // Button type.
+      if (Math.abs(leftPadding - rightPadding) <= 1) {
+        types[index] = ButtonType;
+        return;
+      }
+
+      // Input type with placeholder.
+      if (layout[0].offset <= 2) {
+        types[index] = InputType;
+        return;
+      }
+    }
+
+    cv.imwrite(`test${index}.png`, crop);
+  });
+
+  console.log(types)
+
+  return forItemContours;
+
+  // const features = getFeatures(contour);
+  // const featureDistance = Object.keys(componentFeature).map(component => ({
+  //   component,
+  //   distance: distanceBetween(componentFeature[component], features),
+  // })).sort((a, b) => a.distance - b.distance);
+
+  // contour.type = featureDistance[0].component;
+
+  // console.log(featureDistance)
+
+  // return contour;
 };
 
-module.exports = classifyType;
+module.exports = {
+  containRect,
+  classifyType,
+  isEqualRect,
+};
--- app/scripts/extractRect.js
+++ app/scripts/extractRect.js
@@ -2,10 +2,10 @@ const cv = require("opencv4nodejs");
 const process = require("process");
 const path = require("path");
 const fs = require("fs");
-const classifyType = require("./classifyType");
+const { classifyType, containRect, isEqualRect } = require("./classifyType");
 
 const RED = new cv.Vec(0, 0, 255);
-const shouldWriteOutput = false;
+const shouldWriteOutput = true;
 
 // [ '/usr/local/bin/node',
 //   'extractReact.js',
@@ -13,26 +13,18 @@ const shouldWriteOutput = false;
 const targetFilePath = process.argv[2];
 
 const src = cv.imread(targetFilePath);
-const gray = src.cvtColor(cv.COLOR_RGBA2GRAY);
+const blur = src.gaussianBlur(new cv.Size(5, 5), 0);
+const gray = blur.cvtColor(cv.COLOR_RGBA2GRAY);
 const canny = gray.canny(0, 100);
-const dilate = canny.dilate(
-  cv.getStructuringElement(cv.MORPH_ELLIPSE, new cv.Size(10, 10))
+const kernel = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(15, 5));
+const dilate = canny.morphologyEx(
+  kernel,
+  cv.MORPH_CLOSE,
 );
+
+cv.imwrite('dilate.png', dilate);
+
 let contours = dilate.findContours(cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
-
-if (shouldWriteOutput) {
-  for (let i = 0; i < contours.length; i++) {
-    const contour = contours[i];
-
-    const rect = contour.boundingRect();
-    src.drawRectangle(rect, RED, 2);
-  }
-}
-
-// utils
-const containRect = (a, b) => {
-  return b.x >= a.x && b.y >= a.y && (b.x + b.width) <= (a.x + a.width) && (b.y + b.height) <= (a.y + a.height);
-}
 
 // e.g.:  | |,  | |
 //        |       |
@@ -40,14 +32,19 @@ const inSameRow = (a, b) => {
   if (a.y > b.y) {
     return a.y - b.y < b.height;
   } else {
-    return b.y - a.y < a.height; 
+    return b.y - a.y < a.height;
   }
 }
 
 contours = contours.map(v => v.boundingRect());
 
+// filter outer contour.
+contours = contours.filter(v => v.x + v.y + src.sizes[0] - v.height + src.sizes[1] - v.width > 20);
+// remove duplicated contours.
+contours = contours.filter((v, index) => contours.findIndex(w => isEqualRect(w, v)) === index);
+
 // filter the form item which is direct child contour.
-let formItemContours = contours.filter(v => !contours.some(w => v !== w && containRect(w, v)));
+let formItemContours = contours.filter(v => !contours.some(w => !isEqualRect(v, w) && containRect(w, v)));
 
 // merge form items parts.
 let i = 0;
@@ -63,19 +60,19 @@ while (i < formItemContours.length) {
   const width = merged[merged.length - 1].x + merged[merged.length - 1].width - merged[0].x;
 
   let newContour = formItemContours[i];
-  while(true) {
+  while (true) {
     const toBeMerged = merged.find(v => {
       const distance = v.x - (newContour.x + newContour.width);
-      return distance > 0 && distance < width * 0.05;
+      return distance >= 0 && distance < width * 0.1;
     });
 
     if (toBeMerged) {
-      newContour = {
-        ...newContour,
-        width: toBeMerged.x + toBeMerged.width - newContour.x,
-        y: Math.min(toBeMerged.y, newContour.y),
-        height: Math.max(toBeMerged.y + toBeMerged.height, newContour.y + newContour.height) - Math.min(toBeMerged.y, newContour.y),
-      }
+      newContour = new cv.Rect(
+        newContour.x,
+        Math.min(toBeMerged.y, newContour.y),
+        toBeMerged.x + toBeMerged.width - newContour.x,
+        Math.max(toBeMerged.y + toBeMerged.height, newContour.y + newContour.height) - Math.min(toBeMerged.y, newContour.y)
+      );
 
       const toBeMergedIndex = formItemContours.indexOf(toBeMerged);
       merged = merged.filter(v => v !== toBeMerged);
@@ -89,25 +86,38 @@ while (i < formItemContours.length) {
   i++;
 }
 formItemContours = formItemContours.filter(v => !!v);
-
-// filter form item direct children.
-formItemContours = formItemContours.map(v => {
-  v.children = contours.filter(w => v !== w && containRect(v, w));
-  v.children = v.children.map(v => ({ ...v }));
-  return v;
-})
-
-formItemContours = formItemContours.map(v => classifyType(v));
-formItemContours = formItemContours.map(v => {
-  v.children = undefined;
-  return v;
-});
-
-fs.writeFileSync(targetFilePath.replace(path.extname(targetFilePath), '_bouding.json'), JSON.stringify({
-  size: src.sizes,
-  contours: formItemContours,
-}, null, 2));
+formItemContours = formItemContours.filter(v => !formItemContours.some(w => v !== w && containRect(w, v)));
 
 if (shouldWriteOutput) {
-  cv.imwrite(targetFilePath.replace(path.extname(targetFilePath), `_bouding.${path.extname(targetFilePath)}`), src);
+  for (let i = 0; i < contours.length; i++) {
+    const contour = contours[i];
+
+    src.drawRectangle(contour, RED, 1);
+  }
 }
+
+
+// // filter form item direct children.
+// formItemContours = formItemContours.map(v => {
+//   v.children = contours.filter(w => v !== w && containRect(v, w));
+//   v.children = v.children.map(v => ({ ...v }));
+//   return v;
+// })
+
+formItemContours = classifyType(formItemContours, canny, contours, gray);
+// // formItemContours = formItemContours.map(v => {
+// //   v.children = undefined;
+// //   return v;
+// // });
+
+// fs.writeFileSync(targetFilePath.replace(path.extname(targetFilePath), '_bouding.json'), JSON.stringify({
+//   size: src.sizes,
+//   contours: formItemContours,
+// }, null, 2));
+
+// if (shouldWriteOutput) {
+//   cv.imwrite(targetFilePath.replace(path.extname(targetFilePath), `_bouding.${path.extname(targetFilePath)}`), src);
+// }
+
+cv.imwrite('contour.png', src);
+cv.imwrite('canny.png', canny);
